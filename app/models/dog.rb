98e80aad9ac31b80610f2271a0d557766bf27e25
Samantha brake <sammy.brake@gmail.com>
2018-11-27 17:26:27 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,9 +3,9 @@ class Dog
   @@all = []
 
   def initialize(name, breed, age)
-    @name = name 
-    @breed = breed 
-    @age = age 
+    @name = name
+    @breed = breed
+    @age = age
   end
 
   def self.all?
